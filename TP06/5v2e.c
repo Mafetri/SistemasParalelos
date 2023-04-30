f98f9ab797dcb2d1d1583253cc9ab4a0054db096
Manuel Triñanes <manutritri2@gmail.com>
2023-04-30 12:52:34 -0300

    TP06-05 Ok!

--- TP06/5v2e.c
+++ TP06/5v2e.c
@@ -5,30 +5,27 @@ void main () {
     long num_steps = 800000000;
     double step;
     int i;
-    int max_threads = 8;
-    int array[max_threads][8];
+    int threads_used;    
     double x, pi, sum = 0.0;
 
     step = 1.0/(double) num_steps;
-
-    #pragma omp parallel firstprivate(sum, x, i) num_threads(max_threads)
+    omp_set_dynamic(1);
+    #pragma omp parallel firstprivate(x, i) reduction(+:sum)
     {
         int id = omp_get_thread_num();
         int section_length = num_steps / omp_get_num_threads();
         int start = section_length * id;
         int end = section_length * (id+1);
+        double sum_local;
 
         for (i = start; i < end; i++){
             x = (i + 0.5) * step;
-            sum = sum + 4.0 / (1.0 + x * x);
+            sum_local = sum_local + 4.0 / (1.0 + x * x);
         }
 
-        array[id][0] = sum;
+        sum += sum_local;
     }
 
-    for(int i = 0; i < max_threads; i++) {
-        pi += step * array[i][0];
-    }
-
-    printf("%f\n",pi);
+    pi = step * sum;
+    printf("%f\n", pi);
 }
