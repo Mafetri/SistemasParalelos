49ce592d985c6caa7e528863c0297470c4b5847c
Manuel Triñanes <manutritri2@gmail.com>
2023-06-03 15:01:54 -0700

    Casi listo pa

--- TPGrupal/life.c
+++ TPGrupal/life.c
@@ -6,7 +6,8 @@
 int main(int argc, char *argv[]) {
     FILE *f;
     char *s, *res;
-    int rows, cols, steps, n, i, j;
+    int rows, cols, steps, n, i, j, k;
+    int rank, comm_size, down_rank, up_rank, left_rank, right_rank;
 
     //Controla que se haya ingresado un argumento en la llamada (el archivo con el  patrón de entrada) 
     if (argc != 2) {
@@ -29,8 +30,17 @@ int main(int argc, char *argv[]) {
     }
 
     MPI_Init (&argc, &argv);
-    int rank, comm_size;
+    // MPI Variables
     MPI_Comm grid;
+    MPI_Request send_request;
+    MPI_Request send_request2;
+    MPI_Request recv_request_left;
+    MPI_Request recv_request_right;
+    MPI_Datatype column_type;
+    MPI_Datatype row_type;
+    MPI_Request recv_request_up = MPI_REQUEST_NULL;
+    MPI_Request recv_request_down = MPI_REQUEST_NULL;
+
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
 
@@ -63,10 +73,10 @@ int main(int argc, char *argv[]) {
     int total_size_my_colms = size_my_colms + 2;
     
     //Se reserva memoria dinámica para la matriz de celdas, representada por el arreglo de punteros "old"	
-    char **old;
-    old = malloc(total_size_my_rows * sizeof (char*));
+    char *old[total_size_my_rows];
+    char *old_temp = malloc(total_size_my_rows * total_size_my_colms * sizeof(char));
     for (i = 0; i < total_size_my_rows; i++) {
-        old[i] = (char *) malloc(total_size_my_colms * sizeof (char));
+        old[i] = &old_temp[i * total_size_my_colms];
     }
 
     //Inicializa elementos de la matriz "old" con 0 o 1 segun el patron de entrada 
@@ -95,62 +105,95 @@ int main(int argc, char *argv[]) {
     fclose(f); //Se cierra el archivo 
     free(s); //Se libera la memoria utilizada para recorrer el archivo
     
-    // Envias a la derecha e izquierda la columna correspondiente
-    int left_rank;
-    int right_rank;
-    MPI_Cart_shift(grid, 1, 1, &left_rank, &right_rank);
-    MPI_Datatype column_type;
-    MPI_Type_vector(size_my_rows, 1, total_size_my_colms, MPI_CHAR, &column_type);
-    MPI_Type_commit(&column_type);
-    MPI_Request send_request;
-    MPI_Request send_request2;
-    MPI_Request recv_request_left;
-    MPI_Request recv_request_right;
-    MPI_Isend(&old[1][1], 1, column_type, left_rank, 0, grid, &send_request);
-    MPI_Isend(&old[1][size_my_colms], 1, column_type, right_rank, 1, grid, &send_request2);    
-    MPI_Irecv(&old[1][total_size_my_colms-1], 1, column_type, right_rank, 0, grid, &recv_request_right);
-    MPI_Irecv(&old[1][0], 1, column_type, left_rank, 1, grid, &recv_request_left);
-
-    // Harias tu calculo interno
-    for(i = 1; i < size_my_rows - 1; i++) {
-        for(j = 1; j < size_my_colms - 1; j++){
-            // Tocas a next step
+    // Execution of the life game
+    char *nextStep[total_size_my_rows];
+    char *nextStep_temp = malloc(total_size_my_rows * total_size_my_colms * sizeof(char));
+    for (i = 0; i < total_size_my_rows; i++) {
+        nextStep[i] = &nextStep_temp[i * total_size_my_colms];
+        for(j=0; j < total_size_my_colms; j++){
+            nextStep[i][j] = 0;
         }
     }
 
-    // Esperas recibir las columnas derecha e izquierda
-    MPI_Wait(&recv_request_left, MPI_STATUS_IGNORE);
-    MPI_Wait(&recv_request_right, MPI_STATUS_IGNORE);
+    int c;
+    int cant1;
+    for (c = 0; c < steps; c++){
+        // Envias a la derecha e izquierda la columna correspondiente
+        MPI_Cart_shift(grid, 1, 1, &left_rank, &right_rank);
+        MPI_Type_vector(size_my_rows, 1, total_size_my_colms, MPI_CHAR, &column_type);
+        MPI_Type_commit(&column_type);
+        MPI_Isend(&old[1][1], 1, column_type, left_rank, 0, grid, &send_request);
+        MPI_Isend(&old[1][size_my_colms], 1, column_type, right_rank, 1, grid, &send_request2);    
+        MPI_Irecv(&old[1][total_size_my_colms-1], 1, column_type, right_rank, 0, grid, &recv_request_right);
+        MPI_Irecv(&old[1][0], 1, column_type, left_rank, 1, grid, &recv_request_left);
 
-    // Envias las filas superiores e inferiores
-    int down_rank;
-    int up_rank;
-    MPI_Cart_shift(grid, 0, 1, &up_rank, &down_rank);
-    MPI_Datatype row_type;
-    MPI_Request recv_request_up = MPI_REQUEST_NULL;
-    MPI_Request recv_request_down = MPI_REQUEST_NULL;
-    MPI_Type_vector(total_size_my_colms, 1, 1, MPI_CHAR, &row_type);
-    MPI_Type_commit(&row_type);
-    MPI_Isend(&old[1][0], 1, row_type, up_rank, 0, grid, &send_request);
-    MPI_Isend(&old[size_my_rows][0], 1, row_type, down_rank, 1, grid, &send_request2);    
-    MPI_Irecv(&old[0][0], 1, row_type, up_rank, 1, grid, &recv_request_up);
-    MPI_Irecv(&old[size_my_rows+1][0], 1, row_type, down_rank, 0, grid, &recv_request_down);
+        // Harias tu calculo interno (desde [2][2] a [total_size_my_rows-3][total_size_my_colms-3])
+        for (i = 2; i < size_my_rows; i++) {
+            for (j = 2; j < size_my_colms; j++) {
+                // Fila superior
+                cant1 = old[i-1][j-1] + old[i-1][j] + old[i-1][j+1];
+                // Fila actual
+                cant1 += old[i][j-1] + old[i][j+1];
+                // Fila inferior
+                cant1 += old[i+1][j-1] + old[i+1][j] + old[i+1][j+1];
 
-    // Calculas los de la derecha e izquierda
+                if (old[i][j] == 1) {
+                    // Si tiene 2 o 3 vecinas vivas, sigue viva
+                    nextStep[i][j] = ((cant1 == 2 || cant1 == 3)) ? 1 : 0;
+                } else {
+                    //Si está muerta y tiene 3 vecinas vivas revive
+                    nextStep[i][j] = (cant1 == 3) ? 1 : 0;
+                }
+            }
+        }
 
-    // Esperas recibir las filas superiores e inferiores
-    MPI_Wait(&recv_request_left, MPI_STATUS_IGNORE);
-    MPI_Wait(&recv_request_right, MPI_STATUS_IGNORE);
+        // Esperas recibir las columnas derecha e izquierda
+        MPI_Wait(&recv_request_left, MPI_STATUS_IGNORE);
+        MPI_Wait(&recv_request_right, MPI_STATUS_IGNORE);
 
-    // Calculas las filas superiores e inferiores
+        // Envias las filas superiores e inferiores
+        MPI_Cart_shift(grid, 0, 1, &up_rank, &down_rank);
+        MPI_Type_vector(total_size_my_colms, 1, 1, MPI_CHAR, &row_type);
+        MPI_Type_commit(&row_type);
+        MPI_Isend(&old[1][0], 1, row_type, up_rank, 0, grid, &send_request);
+        MPI_Isend(&old[size_my_rows][0], 1, row_type, down_rank, 1, grid, &send_request2);    
+        MPI_Irecv(&old[0][0], 1, row_type, up_rank, 1, grid, &recv_request_up);
+        MPI_Irecv(&old[size_my_rows+1][0], 1, row_type, down_rank, 0, grid, &recv_request_down);
 
+        // Calculas los de la derecha e izquierda
+        // Harias tu calculo de las columnas exteriores (desde [2][2] a [total_size_my_rows-3][2])
+        for (i = 2; i < size_my_rows; i++) {
+            for(k = 0; k < 2; k++) {
+                // Primera y ultima columna (sin las esquinas) 
+                if(k == 0) {
+                    j = 1;
+                } else {
+                    j = size_my_colms;
+                }
 
-    // Finaliza el ciclo
-    
-    MPI_Barrier(MPI_COMM_WORLD);
-	for (int i = 0; i < comm_size; i++) {
-        if (rank == i) {
-            printf("Soy: %d, derecha: %d izquierda: %d \n", rank, right_rank, left_rank);
+                // Fila superior
+                cant1 = old[i-1][j-1] + old[i-1][j] + old[i-1][j+1];
+                // Fila actual
+                cant1 += old[i][j-1] + old[i][j+1];
+                // Fila inferior
+                cant1 += old[i+1][j-1] + old[i+1][j] + old[i+1][j+1];
+
+                if (old[i][j] == 1) {
+                    // Si tiene 2 o 3 vecinas vivas, sigue viva
+                    nextStep[i][j] = ((cant1 == 2 || cant1 == 3)) ? 1 : 0;
+                } else {
+                    //Si está muerta y tiene 3 vecinas vivas revive
+                    nextStep[i][j] = (cant1 == 3) ? 1 : 0;
+                }
+            }
+        }
+
+        // Esperas recibir las filas superiores e inferiores
+        MPI_Wait(&recv_request_up, MPI_STATUS_IGNORE);
+        MPI_Wait(&recv_request_down, MPI_STATUS_IGNORE);
+
+        if(rank == 0 && c == 0){
+            printf("Soy: %d, arriba: %d abajo: %d \n", rank, up_rank, down_rank);
             for(int k = 0; k < total_size_my_rows; k++) {
                 for(int l = 0; l < total_size_my_colms; l++) {
                     printf("%d, ",old[k][l]);
@@ -159,141 +202,55 @@ int main(int argc, char *argv[]) {
             }
             printf("\n");
         }
-        MPI_Barrier(MPI_COMM_WORLD);
-    }
-    
-    
-    /*
-        char filename[50];
-        sprintf(filename, "subgrid3_%d_%d.out", coords[0], coords[1]);
-        FILE *output = fopen(filename, "w");
-        for(int k = 0; k < total_size_my_rows; k++) {
-            for(int l = 0; l < total_size_my_colms; l++) {
-                fprintf(output, "%d, ",old[k][l]);
+
+        // Calculas las filas superiores e inferiores
+        // Harias tu calculo desde [1][1] a [1][total_size_my_colms-3]
+        // y de [total_size_my_rows-3][1] a [total_size_my_rows-3][total_size_my_colms-3]
+        for (k = 0; k < 2; k++) {
+            // Primera y ultima fila
+            if(k == 0) {
+                i = 1;
+            } else {
+                i = size_my_rows;
             }
-            fprintf(output, "\n");
-        }
-        fprintf(output, "\n");
-    */
 
-    /* Columnas con sendrecv
-    MPI_Wait(&recv_request_left, MPI_STATUS_IGNORE);
-    MPI_Sendrecv(&old[1][1], 1, column_type, left_rank, 0,
-                &old[1][total_size_my_colms - 1], 1, column_type, right_rank, 0,
-                grid, MPI_STATUS_IGNORE);
-    MPI_Sendrecv(&old[1][size_my_colms], 1, column_type, right_rank, 1,
-             &old[1][0], 1, column_type, left_rank, 1,
-             grid, MPI_STATUS_IGNORE);
-    */
+            for (j = 1; j <= size_my_colms; j++) {
+                // Fila superior
+                cant1 = old[i-1][j-1] + old[i-1][j] + old[i-1][j+1];
+                // Fila actual
+                cant1 += old[i][j-1] + old[i][j+1];
+                // Fila inferior
+                cant1 += old[i+1][j-1] + old[i+1][j] + old[i+1][j+1];
 
-    /* Anterior forma de imprimir derecha e izquierda
-    printf("%d rank: %d, recibi de %d y %d \n", size_my_colms, rank, left_rank, right_rank);
-    printf("L   R\n");
-    for(j = 0; j < size_my_rows; j++){
-        printf("%d    ", left_column[j]);
-        printf("%d \n", right_column[j]);
-    }
-    */
-
-
-    // Envias la fila superior e inferior
-    // Calculas tus costados (derecha e izquierda)
-    // Esperas recibir las filas superiores e inferiores
-    // Calculas tus filas superiores e inferiores
-    // Reinicias el ciclo
-
-    /*
-    for (j = 0; j < MATRIX_SIZE; j++) {
-		MPI_Recv(&matrix[0][j], 1, column_type, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-    // WAIT
-    // Calculas con respesto a lo recivido
-
-    MPI_Datatype row;
-    MPI_Type_commit(&row);
-
-    MPI_Type_vector(size_my_colms + 2, 1, 0, MPI_Datatype old_type, MPI_Datatype *newtype)
-
-
-    
-    */
-    
-
-    /*
-    //Copias para hacer el mundo toroidal
-    for (j = 1; j < ncols - 1; j++) { //Copia las columnas
-        old[0][j] = old[nrows - 2][j];
-        old[nrows - 1][j] = old[1][j];
-    }
-
-    for (i = 1; i < nrows - 1; i++) { //Copia las filas
-        old[i][0] = old[i][ncols - 2];
-        old[i][ncols - 1] = old[i][1];
-    }
-
-    //Copia las 4 esquinas
-    old[0][0] = old[nrows - 2][ncols - 2];
-    old[nrows - 1][0] = old[1][ncols - 2];
-    old[0][ncols - 1] = old[nrows - 2][1];
-    old[nrows - 1][ncols - 1] = old[1][1];
-
-    //CALCULAR EL SIGUIENTE ESTADO DE MUNDO
-    char **aux;
-    char **nextStep; //Representa el estado siguiente del mundo
-    nextStep = malloc(nrows * sizeof (char*));
-    aux = malloc(nrows * sizeof (char*));
-
-    for (i = 0; i < nrows; i++)
-        nextStep[i] = (char *) malloc((cols + 2) * sizeof (char));
-    int c;
-    int cant1;
-    for (c = 0; c < steps; c++) {        
-        //Se recorre la matriz y se calcula el valor de su estado siguiente en la matriz nextStep
-        for (i = 1; i <= rows; i++) {
-            for (j = 1; j <= cols; j++) {
-                //Suma las celdas vecinas para saber cuantas están vivas
-                cant1 = old[i - 1][j - 1] + old[i - 1][j] + old[i - 1][j + 1] + old[i][j - 1] + old[i][j + 1] + old[i + 1][j - 1] + old[i + 1][j] + old[i + 1][j + 1];
-
-                if (old[i][j] == 1) //si tiene 2 o 3 vecinas vivas, sigue viva
+                if (old[i][j] == 1) {
+                    // Si tiene 2 o 3 vecinas vivas, sigue viva
                     nextStep[i][j] = ((cant1 == 2 || cant1 == 3)) ? 1 : 0;
-                else
+                } else {
                     //Si está muerta y tiene 3 vecinas vivas revive
                     nextStep[i][j] = (cant1 == 3) ? 1 : 0;
+                }
             }
         }
 
-        //Copia los lados correspondientes en los lados auxiliares
-        for (j = 1; j < ncols - 1; j++) { //Copia columnas
-            nextStep[0][j] = nextStep[nrows - 2][j];
-            nextStep[nrows - 1][j] = nextStep[1][j];
+        // Guarda en old el next step
+        for(i = 0; i < total_size_my_rows; i++){
+            for(j = 0; j < total_size_my_colms; j++){
+                old[i][j] = nextStep[i][j];
+            }
         }
-
-        for (i = 1; i < nrows - 1; i++) {//Copia filas
-            nextStep[i][0] = nextStep[i][ncols - 2];
-            nextStep[i][ncols - 1] = nextStep[i][1];
-        }
-        //Copia las esquinas
-        nextStep[0][0] = nextStep[nrows - 2][ncols - 2];
-        nextStep[nrows - 1][0] = nextStep[1][ncols - 2];
-        nextStep[0][ncols - 1] = nextStep[nrows - 2][1];
-        nextStep[nrows - 1][ncols - 1] = nextStep[1][1];
-
-        //Intercambio de punteros, para realizar el siguiente paso con los nuevos estados calculados   
-        aux = old;
-        old = nextStep;
-        nextStep = aux;
     }
 
-    //IMPRESIÓN DE LA CABECERA Y DEL ÚLTIMO ESTADO LUEGO DE EJECUTAR LOS STEPS
-    printf("cols %d \nrows %d \nsteps %d \n", cols, rows, steps);
-    for (i = 1; i <= rows; i++) {
-        for (j = 1; j <= cols; j++) {
-            printf("%c", old[i][j] == 1 ? 'O' : '.');
+    char filename[50];
+    sprintf(filename, "subgrid_%d_%d.out", coords[0], coords[1]);
+    FILE *output = fopen(filename, "w");
+    for(int k = 1; k <= size_my_rows; k++) {
+        for(int l = 1; l <= size_my_colms; l++) {
+            fprintf(output, "%d, ",old[k][l]);
         }
-        printf("\n");
+        fprintf(output, "\n");
     }
-
-    */
+    fprintf(output, "\n");
+    
     MPI_Finalize();
     return 0;
 }
